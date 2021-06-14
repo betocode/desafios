3dd28ee583edd46f5db8240fd4311a852925d42a
roberto <robertojr_@outlook.com.br>
2021-06-14 15:47:11 -0300

    refactor: healthcheck route now outside of user context

--- app/src/routes.js
+++ app/src/routes.js
@@ -3,10 +3,11 @@ const userController = require("./controllers/userController");
 
 const router = new Router();
 
-// user routes
 router.get("/healthcheck", (req, res) =>
   res.status(200).json({ msg: "App is running" })
 );
+
+// user routes
 router.get("/user/:id", userController.getUserById);
 router.put("/user/:id", userController.updateUserById);
 router.post("/user", userController.createUser);
